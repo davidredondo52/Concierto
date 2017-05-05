7ee326a3d47562ab2c99587bfcdf5616e74b21e1
davidredondo52 <davidredondo52@hotmail.com>
2017-05-05 09:27:30 +0200

    Service Artista

--- Concierto/src/main/java/com/concierto/springmvc/service/ArtistaServiceImpl.java
+++ Concierto/src/main/java/com/concierto/springmvc/service/ArtistaServiceImpl.java
@@ -3,6 +3,8 @@ package com.concierto.springmvc.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +34,11 @@ public class ArtistaServiceImpl implements ArtistaService {
 
 	public List<Artista> findAll() {
 		return artistaDao.findAll();
+	}
+	
+	public Page<Artista> findAllPage(Pageable pageable)
+	{
+		return artistaDao.findAll(pageable);
 	}
 
 	@Override
